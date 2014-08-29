7459d5969355c1ef96407f190d20cf4d59ab0893
Asit Moharna <asitkmoharna@gmail.com>
2014-08-29 16:16:00 +0530

    Set resource directory
    Resource directory is the main folder where model, controller etc. are created.
    Default resource directory is 'app'

--- test/node-cassandra-crudify-test.js
+++ test/node-cassandra-crudify-test.js
@@ -7,6 +7,7 @@ describe("NodeCassandraCrudify", function(){
   describe("#constructor", function(){
     var crudify = new NodeCassandraCrudify('post', {title: 'string', body: 'text'});
     var crudifyWithOptions = new NodeCassandraCrudify('post', {title: 'string', body: 'text'}, {
+      resourceDirectory: 'src',
       resourceFileTypes: ['cql', 'model'],
       templates: {'cql': 'customCql.mustache'}
     });
@@ -26,6 +27,14 @@ describe("NodeCassandraCrudify", function(){
     it("sets model attributes as attribs", function(){
       crudify['attribs'].should.have.property('title', 'string');
       crudify['attribs'].should.have.property('body', 'text');
+    });
+
+    it("sets default resource directory when no options given", function(){
+      crudify.resourceDirectory.should.eql('app');
+    });
+
+    it("sets resource directory from options", function(){
+      crudifyWithOptions.resourceDirectory.should.eql('src');
     });
 
     it("sets default resource file types when no options given", function(){
